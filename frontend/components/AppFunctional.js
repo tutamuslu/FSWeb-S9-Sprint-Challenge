1bcada9f563c3f363efec136558e28fb8121d411
Tugba Muslu <tugba.evli05@gmail.com>
2023-07-10 17:33:57 +0300

    9. hafta challange

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 // önerilen başlangıç stateleri
 const initialMessage = ''
@@ -10,45 +11,116 @@ export default function AppFunctional(props) {
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
 
+  const [state, setState] = useState({
+    message: initialMessage,
+    email:initialEmail,
+    steps:initialSteps,
+    index: initialIndex
+});
+
+const grid = [[1, 1], [2, 1], [3, 1],
+  [1, 2], [2, 2], [3, 2],
+  [1, 3], [2, 3], [3, 3]]
+
   function getXY() {
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
     // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
+
+    return grid[state.index];
+
   }
 
   function getXYMesaj() {
     // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
     // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
     // tamamen oluşturulmuş stringi döndürür.
+
+    const [x, y] = getXY();
+    return `Koordinatlar (${x}, ${y})`
+
   }
 
   function reset() {
     // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
+
+    setState({ 
+      message: initialMessage,
+      email:initialEmail,
+      steps:initialSteps,
+      index: initialIndex})
   }
 
   function sonrakiIndex(yon) {
     // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
     // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
     // şu anki indeksi değiştirmemeli.
+    const [x, y] = getXY();
+    if(yon === "left" && x !==1 ) {
+      setState({...state, index: state.index-1, message:"", steps:state.steps+1})
+    }
+
+    else if(yon === "right" && x !==3) {
+      setState({...state, index: state.index+1, message:"", steps:state.steps+1})
+    }
+
+    else if(yon === "up" && y !==1) {
+      setState({...state, index: state.index-3, message:"", steps:state.steps+1})
+    }
+
+    else if(yon === "down" && y !==3) {
+      setState({...state, index: state.index+3, message:"", steps:state.steps+1})
+    }
+    
+    if(yon === "left" && x ===1 ) {
+      setState({...state, index: state.index-1, message:"Sola gidemezsiniz"})
+    }
+
+    else if(yon === "right" && x ===3) {
+      setState({...state, index: state.index+1, message:"Sağa gidemezsiniz"})
+    }
+
+    else if(yon === "up" && y ===1) {
+      setState({...state, index: state.index-3, message:"Yukarı gidemezsiniz"})
+    }
+
+    else if(yon === "down" && y ===3) {
+      setState({...state, index: state.index+3, message:"Aşağı gidemezsiniz"})
+    }
+    
   }
 
   function ilerle(evt) {
     // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
     // ve buna göre state i değiştirir.
+
+    sonrakiIndex(evt.target.id);
   }
 
   function onChange(evt) {
     // inputun değerini güncellemek için bunu kullanabilirsiniz
+
+    setState({...state, email:evt.target.value})
   }
 
   function onSubmit(evt) {
     // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
+evt.preventDefault();
+const[x,y] = getXY();
+const payload = {x:x, y:y, steps:steps.state , email: state.email}
+
+axios
+.post("http://localhost:9000/api/result", payload)
+.then(res => {
+  setState({...state, message: res.data.message})
+} )
+  
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="coordinates">{getXYMesaj()}</h3>
+        <h3 id="steps">{state.steps}</h3>
       </div>
       <div id="grid">
         {
@@ -63,15 +135,15 @@ export default function AppFunctional(props) {
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={ilerle}>SOL</button>
+        <button id="up" onClick={ilerle}>YUKARI</button>
+        <button id="right" onClick={ilerle}>SAĞ</button>
+        <button id="down" onClick={ilerle}>AŞAĞI</button>
+        <button id="reset" onClick={reset}>reset</button>
       </div>
       <form>
-        <input id="email" type="email" placeholder="email girin"></input>
-        <input id="submit" type="submit"></input>
+        <input id="email" type="email" placeholder="email girin" onChange={onChange}></input>
+        <input id="submit" type="submit" onSubmit={onSubmit}></input>
       </form>
     </div>
   )
